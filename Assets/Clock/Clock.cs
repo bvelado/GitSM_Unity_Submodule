971c359f060dcc29e14c307c16c6aa78b928790f
Baptiste Velado <bvelado@apperture.fr>
2018-04-25 12:26:29 +0200

    Git repo is now at the root of the unity project

---
+++ Assets/Clock/Clock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Clock {
+
+    public static string GetMinutes()
+    {
+        return DateTime.Now.ToString("mm");
+    }
+
+    public static string GetHours()
+    {
+        return DateTime.Now.ToString("HH");
+    }
+}
